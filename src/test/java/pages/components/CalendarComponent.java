cd482e50eea6d4cd29babecc987ffa4f2a4f2b6a
Sofia Kovaleva <kov.sfa@gmail.com>
2025-07-27 14:47:37 +0500

    changes SelenideElement to String for CalendarComponent

--- src/test/java/pages/components/CalendarComponent.java
+++ src/test/java/pages/components/CalendarComponent.java
@@ -1,21 +1,21 @@
 package pages.components;
 
-import com.codeborne.selenide.SelenideElement;
-
 import static com.codeborne.selenide.Selenide.$;
 
 public class CalendarComponent {
 
-    SelenideElement dateOfBirth = $("#dateOfBirthInput"),
-            monthOfBirth = $(".react-datepicker__month-select"),
-            yearOfBirth = $(".react-datepicker__year-select"),
-            dayOfBirth = $(".react-datepicker__day--0");
+    String dateOfBirth = "#dateOfBirthInput",
+            monthOfBirth = ".react-datepicker__month-select",
+            yearOfBirth = ".react-datepicker__year-select",
+            dayOfBirth = ".react-datepicker__day--0";
 
 
     public void setDate(String day, String month, String year) {
         $(dateOfBirth).click();
         $(monthOfBirth).selectOptionByValue(month);
         $(yearOfBirth).selectOptionByValue(year);
-        $(dayOfBirth + day).click();
+
+        String paddedDay = String.format("%02d", Integer.parseInt(day));
+        $(dayOfBirth + paddedDay).click();
     }
 }
